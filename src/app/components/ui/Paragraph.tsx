89d2471f4cbc8891155ddec5df04bcce56447790
MikeAriyo <ariyoadekola2@gmail.com>
2023-03-27 19:46:17 +0100

    added style variants to paragraph file

--- src/app/components/ui/Paragraph.tsx
+++ src/app/components/ui/Paragraph.tsx
@@ -1,9 +1,38 @@
-import { FC } from "react";
+import { FC, forwardRef, HTMLAttributes } from "react";
+import { cva, VariantProps } from "class-variance-authority";
+import { cn } from "./lib/utils";
 
-interface ParagraphProps {}
+const paragraphVariants = cva(
+  "max-w-prose text-slate-700 dark:text-slate-300 mb-2 text-center",
+  {
+    variants: {
+      size: {
+        default: "text-base sm:text-lg",
+        sm: "text-sm sm:text-base",
+      },
+    },
+    defaultVariants: {
+      size: "default",
+    },
+  }
+);
 
-const Paragraph: FC<ParagraphProps> = ({}) => {
-  return <div>Paragraph</div>;
-};
+interface ParagraphProps
+  extends HTMLAttributes<HTMLParagraphElement>,
+    VariantProps<typeof paragraphVariants> {}
+
+const Paragraph = forwardRef<HTMLParagraphElement, ParagraphProps>(
+  ({ className, size, children, ...props }, ref) => {
+    return (
+      <p
+        ref={ref}
+        {...props}
+        className={cn(paragraphVariants({ size, className }))}
+      ></p>
+    );
+  }
+);
+
+Paragraph.displayName = "Paragraph";
 
 export default Paragraph;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
+import Paragraph from "./components/ui/Paragraph";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main className="text-red-700 text-[25px]">hello world</main>;
+  return (
+    <main className="text-red-700 text-[25px]">
+      <Paragraph />
+    </main>
+  );
 }
