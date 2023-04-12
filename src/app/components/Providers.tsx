b09cdfdeb806f337101e55d478334a40812698d3
MikeAriyo <ariyoadekola2@gmail.com>
2023-04-12 16:33:23 +0100

    next theme provider

--- src/app/components/Providers.tsx
+++ src/app/components/Providers.tsx
@@ -1,9 +1,15 @@
-import { FC } from "react";
+"use client"; // declare as a client component
+import { ThemeProvider } from "next-themes";
+import { FC, ReactNode } from "react";
 
-interface ProviderProps {}
-
-const Providers: FC<ProviderProps> = ({}) => {
-  return <div>Providers</div>;
+const Providers = ({ children }: { children: ReactNode }) => {
+  //specifies that component receives children as props which is a react node
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+    ></ThemeProvider>
+  );
 };
-
 export default Providers;
