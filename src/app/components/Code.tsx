87a94a07a5ed1a25bb6e7d55a20715a1a55b67e1
MikeAriyo <ariyoadekola2@gmail.com>
2023-04-22 22:44:34 +0100

    logic for code animation

---
+++ src/app/components/Code.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { FC, useEffect, useState } from "react";
+import { type Language } from "prism-react-renderer";
+import { useTheme } from "next-themes";
+
+interface CodeProps {
+  code: string;
+  show: boolean;
+  language: Language;
+  animationDelay?: number;
+  animated?: boolean;
+}
+
+const Code: FC<CodeProps> = ({
+  code,
+  language,
+  show,
+  animated,
+  animationDelay,
+}) => {
+  const { theme: applicationTheme } = useTheme();
+  const [text, setText] = useState(animated ? "" : code);
+
+  useEffect(() => {
+    if (show && animated) {
+      let i = 0;
+      setTimeout(() => {
+        const intervalId = setInterval(() => {
+          setText(code.slice(0, i));
+          i++;
+          if (i > code.length) {
+            clearInterval(intervalId);
+          }
+        }, 15);
+
+        return () => clearInterval(intervalId);
+      }, animationDelay || 150);
+    }
+  }, [code, show, animated, animationDelay]);
+
+  //calculate number of lines
+
+  return <div>Code</div>;
+};
+
+export default Code;
--- src/app/components/DocumentationTabs.tsx
+++ src/app/components/DocumentationTabs.tsx
@@ -1,7 +1,21 @@
+import { TabsContent } from "@radix-ui/react-tabs";
 import { FC } from "react";
+import { Tabs, TabsList, TabsTrigger } from "./ui/Tabs";
 
 const DocumentationTabs: FC = ({}) => {
-  return <div>DocumentationTabs</div>;
+  return (
+    <Tabs defaultValue="nodejs" className="max-w-2xl w-full">
+      <TabsList>
+        <TabsTrigger value="nodejs">NodeJS</TabsTrigger>
+        <TabsTrigger value="python">Python</TabsTrigger>
+      </TabsList>
+
+      <TabsContent value="nodejs">
+        <Code />
+      </TabsContent>
+      <TabsContent value="python"></TabsContent>
+    </Tabs>
+  );
 };
 
 export default DocumentationTabs;
