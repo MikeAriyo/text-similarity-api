8f4bc2122e006f31fec50e6779b6f812b83a028d
MikeAriyo <ariyoadekola2@gmail.com>
2023-04-23 00:10:27 +0100

    working around google authentication provider ini next-auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,33 @@
 import { NextAuthOptions } from "next-auth";
+import { db } from "./db";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions : NextAuthOptions = {
-    adapter: PrismaAdapter(db)
+function getGoogleCredentials() {
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+  const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+
+  if (!clientId || clientId.length === 0) {
+    throw new Error("No clientID for google provider set");
+  }
+
+  if (!clientSecret || clientSecret.length === 0) {
+    throw new Error("No clientSecret for google provider set");
+  }
 }
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(db),
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+};
