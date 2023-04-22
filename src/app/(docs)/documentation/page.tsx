39f9f8899cf976163d5c389853da9e393a694db3
MikeAriyo <ariyoadekola2@gmail.com>
2023-04-22 22:25:49 +0100

    radix tabs component

--- src/app/(docs)/documentation/page.tsx
+++ src/app/(docs)/documentation/page.tsx
@@ -2,6 +2,8 @@ import { FC } from "react";
 
 import type { Metadata } from "next";
 import LargeHeading from "@/app/components/ui/LargeHeading";
+import Paragraph from "@/app/components/ui/Paragraph";
+import DocumentationTabs from "@/app/components/DocumentationTabs";
 
 export const metadata: Metadata = {
   title: "Similarity API | Documentation",
@@ -13,6 +15,9 @@ const page: FC = ({}) => {
     <div className="container max-w-7xl mx-auto mt-12">
       <div className="flex flex-col items-center gap-6">
         <LargeHeading>Making a Request</LargeHeading>
+        <Paragraph>api/v1/similarity</Paragraph>
+
+        <DocumentationTabs />
       </div>
     </div>
   );
