263411f77e5d8f3c4f572dc0e63f9b894e67331c
MikeAriyo <ariyoadekola2@gmail.com>
2023-05-07 01:49:13 +0100

    requestApiKey component

--- src/app/components/RequestApiKey.tsx
+++ src/app/components/RequestApiKey.tsx
@@ -1,9 +1,39 @@
 "use client";
 
-import { FC, useState } from "react";
+import { FC, FormEvent, useState } from "react";
+import { toast } from "./ui/toast";
 
 const RequestApiKey: FC = ({}) => {
   const [isCreating, setIsCreating] = useState<boolean>(false);
+
+  const [apiKey, setApiKey] = useState<string | null>(null);
+
+  const createNewApiKey = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setIsCreating(true);
+    try {
+      const generatedApiKey = await createAoiKey();
+    } catch (err) {
+      if (err instanceof Error) {
+        toast({
+          title: "Error",
+          message: err.message,
+          type: "error",
+        });
+
+        return;
+      }
+
+      toast({
+        title: "Error",
+        message: "Something went wrong",
+        type: "error",
+      });
+    }
+    setIsCreating(false);
+  };
+
   return <div>RequestApiKey</div>;
 };
 
